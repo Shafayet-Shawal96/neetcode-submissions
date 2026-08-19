545c660abe4d4c6ebb8abb13b388821b60afd696
S. M Shawal Chowdhury <62823989+Shafayet-Shawal96@users.noreply.github.com>
2026-08-19 22:58:48 +0600

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const indics={}
+
+        for(let i=0; i<nums.length; i++){
+            const complementNum = target-nums[i]
+            if(indics[complementNum]!==undefined && indics[complementNum]!=i){
+                return i>indics[complementNum]? [indics[complementNum],i]:[i,indics[complementNum]]
+            }
+            indics[nums[i]]=i
+        }
+
+        return []
+    }
+}

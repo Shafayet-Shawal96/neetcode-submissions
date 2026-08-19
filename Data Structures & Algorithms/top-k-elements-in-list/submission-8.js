2685c35b09e69d48ddf5b29eac7ec94ce27b49af
S. M Shawal Chowdhury <62823989+Shafayet-Shawal96@users.noreply.github.com>
2026-08-20 00:43:40 +0600

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count = {}
+
+        for(let i=0;i<nums.length;i++){
+            count[nums[i]] = (count[nums[i]]||0) + 1
+        }
+
+        const freq = Array.from({ length: nums.length + 1 }, () => []);
+        for (const n in count) {
+            freq[count[n]].push(parseInt(n));
+        }
+
+        const res = [];
+        for (let i = freq.length - 1; i > 0; i--) {
+            for (const n of freq[i]) {
+                res.push(n);
+                if (res.length === k) {
+                    return res;
+                }
+            }
+        }
+    }
+}
